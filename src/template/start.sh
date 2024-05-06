49f72bfb53ded8d00618bda8fa811ca4bc75d6a7
Pierre <pierre@ideestore.fr>
2024-05-06 20:07:47 +0200

    Update start.sh

--- src/template/start.sh
+++ src/template/start.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 if [[ ! -f server.jar ]]; then
-
    echo -e "\e[31mServer jar not found, \nPlease restart the CLI !\e[0m"
 else
    echo -e "\e[32mServer jar found, \nStarting server...\e[0m"
